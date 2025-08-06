1e5b56729cc2193480da03c865159ac71c83cbc3
millerladino <millerladino@gmail.com>
2025-08-06 10:43:43 -0500

    se agrego experiencia, proyectos y responsive

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from .models import Projects, Experience, About
 def home(request):
     # Obtener todos los proyectos y paginarlos
     projects_list = Projects.objects.all()
-    paginator = Paginator(projects_list, 6)  # Mostrar 3 proyectos por página
+    paginator = Paginator(projects_list, 5)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     
